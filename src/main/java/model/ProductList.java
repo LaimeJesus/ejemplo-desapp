e02fb2cacac5cab4cc264bc650547e79aaf40da0
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-03-25 16:37:49 -0300

    Ahora es posible agregar y eliminar productos de la lista

--- src/main/java/model/ProductList.java
+++ src/main/java/model/ProductList.java
@@ -1,7 +1,10 @@
 package model;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
+import exceptions.ProductDoesNotExistOnListException;
+import exceptions.ProductIsAlreadySelectedException;
 import util.Money;
 
 public class ProductList {
@@ -10,17 +13,46 @@ public class ProductList {
 	String name;
 	//ArrayList<Umbral> umbrals;
 	
-	Money totalAmount;
-	HashMap<Product , Integer> allProducts;
+	Money totalAmount = new Money(0,0);
+	List<SelectedProduct> allProducts = new ArrayList<SelectedProduct>();
 	
 	
-	
-	
-	public void addProduct(Product product , Integer howMany) {
-		if ( ! allProducts.containsKey(product)) {
-			allProducts.put(product, howMany);
+	public void selectProduct(Product product , Integer howMany) throws ProductIsAlreadySelectedException {
+		if (! this.thisProductIsSelected(product)) {
+			this.addProductToList(new SelectedProduct(product, howMany));
 			this.updateAmount( this.calculateAmount(product.getPrice() , howMany) );
-		}	
+		} else {
+			throw new ProductIsAlreadySelectedException("El producto que intenta seleccionar, ya se encuentra dentro del listado");
+		}
+	}
+	
+	public boolean thisProductIsSelected (Product someProduct) {
+		for (SelectedProduct currentProduct : allProducts) {
+			if (currentProduct.getProduct().equals(someProduct)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean isEmpty() {
+		return this.allProducts.isEmpty();
+	}
+	
+	public void addProductToList (SelectedProduct newProduct) {
+		this.allProducts.add(newProduct);
+	}
+	
+	public void deleteProductFromList (Product productToRemove) {
+		this.allProducts.remove(productToRemove);
+	}
+	
+	public void removeProduct(Product productToRemove) throws ProductDoesNotExistOnListException {
+		if (this.thisProductIsSelected(productToRemove)) {
+			this.deleteProductFromList(productToRemove);
+		} else {
+			throw new ProductDoesNotExistOnListException("El producto que intenta eliminar no se encuentra seleccionado");
+		}
 	}
 	
 	public void updateAmount(Money newAmount) {
