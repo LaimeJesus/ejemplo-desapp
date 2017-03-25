fce98eee478a8d6239fc2d71948f20a1cc96b447
jesus <laimejesu@gmail.com>
2017-03-25 16:30:40 -0300

    adding userbuilder to utils

---
+++ src/main/java/util/UserBuilder.java
@@ -0,0 +1,40 @@
+package util;
+
+import model.User;
+
+public class UserBuilder {
+
+	private User user;
+
+	public UserBuilder(){
+		this.user = new User();
+	}
+	
+	private User getUser() {
+		return this.user;
+	}
+
+	public UserBuilder withPassword(Password aPassword) {
+		this.getUser().setPassword(aPassword);
+		return this;
+	}
+
+	public UserBuilder withAddress(Address anAddress) {
+		this.getUser().setAddress(anAddress);
+		return this;
+	}
+
+	public User build() {
+		return this.getUser();
+	}
+
+	public UserBuilder withUsername(String anUsername) {
+		this.getUser().setUsername(anUsername);
+		return this;
+	}
+
+	public UserBuilder withEmail(String anEmail) {
+		this.getUser().setEmail(anEmail);
+		return this;
+	}
+}
