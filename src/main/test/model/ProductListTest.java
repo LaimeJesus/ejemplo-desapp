41f65a78e7d7516ed7bead562cc6cc5843978307
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-03-25 18:05:26 -0300

    Continuando los tests

--- src/main/test/model/ProductListTest.java
+++ src/main/test/model/ProductListTest.java
@@ -1,5 +1,10 @@
 package model;
 
+import org.mockito.*;
+
+import exceptions.ProductIsAlreadySelectedException;
+import util.Money;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -13,6 +18,22 @@ public class ProductListTest {
 		
 		assertTrue(someProductList.isEmpty());
 		
+	}
+	
+	@Test
+	public void testWhenMyProductListAddsAValidNewProductMyListIsIncreased() throws ProductIsAlreadySelectedException {
+		
+		ProductList someProductList = new ProductList();
+		
+		Product aProduct = Mockito.mock(Product.class);
+		Money aMoney = Mockito.mock(Money.class);
+		
+		Mockito.when(aMoney.times(3)).thenReturn(aMoney);
+		Mockito.when(aProduct.getPrice()).thenReturn(aMoney);
+		
+		someProductList.selectProduct(aProduct, 3);
+		
+		assertTrue(! someProductList.isEmpty());
 	}
 
 }
