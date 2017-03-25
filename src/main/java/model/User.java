a608e9ae4a2329f8a0b996c3c117d1e81b427a54
jesus <laimejesu@gmail.com>
2017-03-25 16:28:45 -0300

    changing address, password, profile

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -2,26 +2,89 @@ package model;
 
 import java.util.ArrayList;
 
+import util.Address;
+import util.Password;
+
 public class User {
 	
-	String username;
-	String email;
-	String password;
-	String address;
+	private String username;
+	private String email;
+
+	private Profile profile;	
+	private Address address;
+	private Password password;
+	private ArrayList<ProductList> allLists;
 	
-	Profile profile;
 	
-	ArrayList<ProductList> allLists;
-	
-	
-	public User(String newUsername , String newEmail , String newPassword , String newAddress) {
-		this.username = newUsername;
-		this.email = newEmail;
-		this.password = newPassword;
-		this.address = newAddress;
-		this.allLists = new ArrayList<ProductList>();
+	public User(String newUsername , String newEmail , Password newPassword , Address newAddress) {
+		this.setUsername(newUsername);
+		this.setEmail(newEmail);
+		this.setPassword(newPassword);
+		this.setAddress(newAddress);
+		this.setProfile(new Profile());
+		this.setAllLists(new ArrayList<ProductList>());		
 	}
-	
-	
+
+	public User() {
+		this.setAllLists(new ArrayList<ProductList>());
+		this.setProfile(new Profile());
+	}
+
+
+	public Profile getProfile() {
+		return this.profile;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+
+
+	public Address getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+
+	public Password getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(Password password) {
+		this.password = password;
+	}
+
+	public ArrayList<ProductList> getAllLists() {
+		return allLists;
+	}
+
+	public void setAllLists(ArrayList<ProductList> allLists) {
+		this.allLists = allLists;
+	}
 
 }
