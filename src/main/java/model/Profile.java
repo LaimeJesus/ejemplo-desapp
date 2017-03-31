d40cca3cea7e32f4c4b04c4afbb1297be428c0e1
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-03-31 02:50:35 -0300

    decouple user from its profile

--- src/main/java/model/Profile.java
+++ src/main/java/model/Profile.java
@@ -1,5 +1,55 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import exceptions.ProductListAlreadyCreatedException;
+import util.Address;
+
 public class Profile {
 
+	
+	private Address address;
+	private ArrayList<ProductList> allProductLists;
+	
+	public Profile() {
+		allProductLists = new ArrayList<ProductList>();
+	}
+	
+	
+	
+	public void createProductList(String newName) throws ProductListAlreadyCreatedException {
+		if (this.listAlreadyExist(newName)) {
+			throw new ProductListAlreadyCreatedException("Ya existe una lista con el mismo nombre");
+		} else {
+			ProductList newProductList = new ProductList(newName);
+			allProductLists.add(newProductList);
+		}
+	}
+	
+	public boolean listAlreadyExist(String name) {
+		for (ProductList currentProductList : this.allProductLists) {
+			if (currentProductList.getName().equals(name)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public Address getAddress() {
+		return this.address;
+	}
+	
+	public void setAddress(Address newAddress) {
+		this.address = newAddress;
+	}
+	
+	public ArrayList<ProductList> getAllProductList() {
+		return this.allProductLists;
+	}
+	
+	public void setAllLists(ArrayList<ProductList> newProductLists) {
+		this.allProductLists = newProductLists;
+	}
+	
 }
