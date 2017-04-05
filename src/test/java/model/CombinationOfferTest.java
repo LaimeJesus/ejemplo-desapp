5752efcc43fcab5735bfd85caa5689a1f0828927
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-04-05 00:54:48 -0300

    Refactor to method - possible Builder in proccess

--- src/test/java/model/CombinationOfferTest.java
+++ src/test/java/model/CombinationOfferTest.java
@@ -2,6 +2,8 @@ package model;
 
 import static org.junit.Assert.*;
 
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -16,15 +18,19 @@ public class CombinationOfferTest {
 		
 		Product firstProductMock = Mockito.mock(Product.class);
 		Product secondProductMock = Mockito.mock(Product.class);
+		DateTime today = DateTime.now();
+		DateTime tomorrow = today.plusDays(1);
+		Interval anIntervalMock = new Interval(today, tomorrow);
+		ProductList aProductListMock = Mockito.mock(ProductList.class);
 		
 		Mockito.when(firstProductMock.getPrice()).thenReturn(new Money(24,56));
 		Mockito.when(secondProductMock.getPrice()).thenReturn(new Money(17,21));
 		
-		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15);
+		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15, anIntervalMock);
 		
 		Money expected = new Money(41,77);
 		
-		assertEquals(aOffer.getPreviousPrice(), expected);
+		assertEquals(aOffer.getPreviousPrice(aProductListMock), expected);
 		
 	}
 	
@@ -33,15 +39,19 @@ public class CombinationOfferTest {
 		
 		Product firstProductMock = Mockito.mock(Product.class);
 		Product secondProductMock = Mockito.mock(Product.class);
+		DateTime today = DateTime.now();
+		DateTime tomorrow = today.plusDays(1);
+		Interval anIntervalMock = new Interval(today, tomorrow);
+		ProductList aProductListMock = Mockito.mock(ProductList.class);
 		
 		Mockito.when(firstProductMock.getPrice()).thenReturn(new Money(24,56));
 		Mockito.when(secondProductMock.getPrice()).thenReturn(new Money(17,21));
 		
-		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15);
+		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15,anIntervalMock);
 		
 		Money expected = new Money(6,30);
 		
-		assertEquals(aOffer.getDiscount(aOffer.getDiscountRate()), expected);
+		assertEquals(aOffer.getDiscount(aOffer.getDiscountRate() , aProductListMock), expected);
 		
 	}
 	
@@ -50,16 +60,20 @@ public class CombinationOfferTest {
 		
 		Product firstProductMock = Mockito.mock(Product.class);
 		Product secondProductMock = Mockito.mock(Product.class);
+		DateTime today = DateTime.now();
+		DateTime tomorrow = today.plusDays(1);
+		Interval anIntervalMock = new Interval(today, tomorrow);
+		ProductList aProductListMock = Mockito.mock(ProductList.class);
 		
 		Mockito.when(firstProductMock.getPrice()).thenReturn(new Money(24,56));
 		Mockito.when(secondProductMock.getPrice()).thenReturn(new Money(17,21));
 		
-		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15);
+		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15, anIntervalMock);
 		
 		Money expected = new Money(35,47);
 		
 		try {
-			assertEquals(aOffer.getFinalPrice(), expected);
+			assertEquals(aOffer.getFinalPrice(aProductListMock), expected);
 		} catch (MoneyCannotSubstractException e) {
 			fail();
 		}
@@ -73,11 +87,14 @@ public class CombinationOfferTest {
 		
 		Product firstProductMock = Mockito.mock(Product.class);
 		Product secondProductMock = Mockito.mock(Product.class);
+		DateTime today = DateTime.now();
+		DateTime tomorrow = today.plusDays(1);
+		Interval anIntervalMock = new Interval(today, tomorrow);
 		
 		Mockito.when(firstProductMock.getPrice()).thenReturn(new Money(24,56));
 		Mockito.when(secondProductMock.getPrice()).thenReturn(new Money(17,21));
 		
-		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15);
+		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15, anIntervalMock);
 		
 		try {
 			someProductList.selectProduct(firstProductMock, 1);
@@ -98,11 +115,14 @@ public class CombinationOfferTest {
 		
 		Product firstProductMock = Mockito.mock(Product.class);
 		Product secondProductMock = Mockito.mock(Product.class);
+		DateTime today = DateTime.now();
+		DateTime tomorrow = today.plusDays(1);
+		Interval anIntervalMock = new Interval(today, tomorrow);
 		
 		Mockito.when(firstProductMock.getPrice()).thenReturn(new Money(24,56));
 		Mockito.when(secondProductMock.getPrice()).thenReturn(new Money(17,21));
 		
-		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15);
+		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15, anIntervalMock);
 		
 		try {
 			someProductList.selectProduct(firstProductMock, 1);
@@ -116,6 +136,78 @@ public class CombinationOfferTest {
 			fail();
 		};
 		
+	}
+	
+	@Test
+	public void testWhenTheTwoProductsHaveBeenSelectedThenTheProductListMeetsTheRequirements() {
+		
+		ProductList someProductList = new ProductList("Example List");
+		
+		Product firstProductMock = Mockito.mock(Product.class);
+		Product secondProductMock = Mockito.mock(Product.class);
+		DateTime today = DateTime.now();
+		DateTime tomorrow = today.plusDays(1);
+		Interval anIntervalMock = new Interval(today, tomorrow);
+		
+		Mockito.when(firstProductMock.getPrice()).thenReturn(new Money(24,56));
+		Mockito.when(secondProductMock.getPrice()).thenReturn(new Money(17,21));
+		
+		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15, anIntervalMock);
+		
+		try {
+			someProductList.selectProduct(firstProductMock, 3);
+			someProductList.selectProduct(secondProductMock, 2);
+			
+			assertTrue(aOffer.meetRequirements(someProductList));
+			
+		} catch (ProductIsAlreadySelectedException e) {
+		};
+		
+	}
+	
+	@Test
+	public void testWhenTheTwoProductsHaveNotBeenSelectedThenTheProductListDoesNotMeetsTheRequirements() {
+		
+		ProductList someProductList = new ProductList("Example List");
+		
+		Product firstProductMock = Mockito.mock(Product.class);
+		Product secondProductMock = Mockito.mock(Product.class);
+		DateTime today = DateTime.now();
+		DateTime tomorrow = today.plusDays(1);
+		Interval anIntervalMock = new Interval(today, tomorrow);
+		
+		Mockito.when(firstProductMock.getPrice()).thenReturn(new Money(24,56));
+		Mockito.when(secondProductMock.getPrice()).thenReturn(new Money(17,21));
+		
+		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15, anIntervalMock);
+
+		assertFalse(aOffer.meetRequirements(someProductList));
+		
+	}
+	
+	@Test
+	public void testWhenOneOfThemIsSelectedThenTheProductListDoesNotMeetsTheRequirements() {
+		
+		ProductList someProductList = new ProductList("Example List");
+		
+		Product firstProductMock = Mockito.mock(Product.class);
+		Product secondProductMock = Mockito.mock(Product.class);
+		DateTime today = DateTime.now();
+		DateTime tomorrow = today.plusDays(1);
+		Interval anIntervalMock = new Interval(today, tomorrow);
+		
+		Mockito.when(firstProductMock.getPrice()).thenReturn(new Money(24,56));
+		Mockito.when(secondProductMock.getPrice()).thenReturn(new Money(17,21));
+		
+		CombinationOffer aOffer = new CombinationOffer(firstProductMock, secondProductMock, 15, anIntervalMock);
+
+		try {
+			someProductList.selectProduct(firstProductMock, 3);
+			
+			assertFalse(aOffer.meetRequirements(someProductList));
+			
+		} catch (ProductIsAlreadySelectedException e) {
+		};
 	}
 
 }
