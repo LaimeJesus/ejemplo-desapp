6c5334c2a3d1ccb968814590a69c7ae326f40f99
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-04-06 17:11:35 -0300

    some tests over CategoryOffer

--- src/test/java/model/CategoryOfferTest.java
+++ src/test/java/model/CategoryOfferTest.java
@@ -2,11 +2,14 @@ package model;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.joda.time.DateTime;
 import org.joda.time.Interval;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import antlr.collections.List;
 import model.offers.CategoryOffer;
 import util.Category;
 import util.Money;
@@ -28,6 +31,22 @@ public class CategoryOfferTest {
 		Money expected = new Money(156,76);
 		
 		assertEquals(expected, aCategoryOffer.getPreviousPrice(aProductListMock));
+		
+	}
+	
+	@Test
+	public void testWhenAListHasAtLeastOneProductOfTheCategoryThenTheOfferCanBeApplied() {
+		
+		ProductList aProductListMock = Mockito.mock(ProductList.class);
+		DateTime today = DateTime.now();
+		DateTime tomorrow = today.plusDays(1);
+		Interval anInterval = new Interval(today, tomorrow);
+		
+		CategoryOffer aCategoryOffer = new CategoryOffer(15, anInterval, Category.Baked);
+		
+		aCategoryOffer.meetRequirements(aProductListMock);
+		
+		Mockito.verify(aProductListMock).getAllProducts();
 		
 	}
 
