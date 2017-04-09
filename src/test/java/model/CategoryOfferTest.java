ac9914eef726a92f56bf5b6f66eda24438e51e74
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-04-09 17:33:21 -0300

    testWhenAListHasNoneProductOfTheCategoryThenTheOfferCanNotBeApplied

--- src/test/java/model/CategoryOfferTest.java
+++ src/test/java/model/CategoryOfferTest.java
@@ -3,6 +3,7 @@ package model;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import org.joda.time.DateTime;
 import org.joda.time.Interval;
@@ -10,6 +11,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import antlr.collections.List;
+import exceptions.ProductIsAlreadySelectedException;
 import model.offers.CategoryOffer;
 import util.Category;
 import util.Money;
@@ -37,16 +39,55 @@ public class CategoryOfferTest {
 	@Test
 	public void testWhenAListHasAtLeastOneProductOfTheCategoryThenTheOfferCanBeApplied() {
 		
-		ProductList aProductListMock = Mockito.mock(ProductList.class);
+		ProductList aProductList = new ProductList();
 		DateTime today = DateTime.now();
 		DateTime tomorrow = today.plusDays(1);
 		Interval anInterval = new Interval(today, tomorrow);
 		
-		CategoryOffer aCategoryOffer = new CategoryOffer(15, anInterval, Category.Baked);
+		CategoryOffer aCategoryOffer = new CategoryOffer(15, anInterval, Category.Drink);
 		
-		aCategoryOffer.meetRequirements(aProductListMock);
+		Product aProductMock = Mockito.mock(Product.class);
+		Mockito.when(aProductMock.getCategory()).thenReturn(Category.Drink);
+		Mockito.when(aProductMock.getPrice()).thenReturn(new Money(2,3));
+		Product a2ProductMock = Mockito.mock(Product.class);
+		Mockito.when(a2ProductMock.getCategory()).thenReturn(Category.Cleaning);
+		Mockito.when(a2ProductMock.getPrice()).thenReturn(new Money(3,2));
 		
-		Mockito.verify(aProductListMock).getAllProducts();
+		try {
+			aProductList.selectProduct(aProductMock, 2);
+			aProductList.selectProduct(a2ProductMock, 3);
+			
+			assertTrue(aCategoryOffer.meetRequirements(aProductList));
+		} catch (ProductIsAlreadySelectedException e) {
+		}
+		
+	}
+	
+	@Test
+	public void testWhenAListHasNoneProductOfTheCategoryThenTheOfferCanNotBeApplied()  {
+		
+		ProductList aProductList = new ProductList();
+		DateTime today = DateTime.now();
+		DateTime tomorrow = today.plusDays(1);
+		Interval anInterval = new Interval(today, tomorrow);
+		
+		CategoryOffer aCategoryOffer = new CategoryOffer(15, anInterval, Category.Drink);
+		
+		Product aProductMock = Mockito.mock(Product.class);
+		Mockito.when(aProductMock.getCategory()).thenReturn(Category.Dairy);
+		Mockito.when(aProductMock.getPrice()).thenReturn(new Money(2,3));
+		Product a2ProductMock = Mockito.mock(Product.class);
+		Mockito.when(a2ProductMock.getCategory()).thenReturn(Category.Cleaning);
+		Mockito.when(a2ProductMock.getPrice()).thenReturn(new Money(3,2));
+		
+		try {
+			aProductList.selectProduct(aProductMock, 2);
+			aProductList.selectProduct(a2ProductMock, 3);
+			
+			assertFalse(aCategoryOffer.meetRequirements(aProductList));
+		} catch (ProductIsAlreadySelectedException e) {
+		}
+		
 		
 	}
 
