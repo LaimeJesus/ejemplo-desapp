0a600dc20583a35cc442006f0c0f2f647643a0a0
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-04-13 15:16:21 -0300

    testWhenGivenALimitedProcessedDictionaryDataThenICanGetALimitedRecommendation

--- src/test/java/model/recommendations/SameCategoryRecommendationTest.java
+++ src/test/java/model/recommendations/SameCategoryRecommendationTest.java
@@ -95,5 +95,48 @@ public class SameCategoryRecommendationTest {
 		
 		
 	}
+	
+	@Test
+	public void testWhenGivenALimitedProcessedDictionaryDataThenICanGetALimitedRecommendation() {
+		
+		Product productOneMock = Mockito.mock(Product.class);
+		Product productTwoMock = Mockito.mock(Product.class);
+		Product productThreeMock = Mockito.mock(Product.class);
+		Product productFourMock = Mockito.mock(Product.class);
+		Product productFiveMock = Mockito.mock(Product.class);
+		
+		List<Product> products = new ArrayList<Product>();
+		List<Product> expected = new ArrayList<Product>();
+		
+		products.add(productOneMock);
+		products.add(productTwoMock);
+		products.add(productThreeMock);
+		products.add(productFourMock);
+		products.add(productFiveMock);
+		
+		expected.add(productOneMock);
+		expected.add(productFourMock);
+		
+		
+		Mockito.when(productOneMock.getCategory()).thenReturn(Category.Drink);
+		Mockito.when(productTwoMock.getCategory()).thenReturn(Category.Cleaning);
+		Mockito.when(productThreeMock.getCategory()).thenReturn(Category.Dairy);
+		Mockito.when(productFourMock.getCategory()).thenReturn(Category.Drink);
+		Mockito.when(productFiveMock.getCategory()).thenReturn(Category.Baked);
+		
+		
+		SameCategoryRecommendation scr = new SameCategoryRecommendation();
+		
+		scr.setElementsToProcess(products);
+		
+		scr.processData();
+		
+		Recommendation aRecommendation = scr.generateRecommendation(productOneMock, 1);
+		
+		assertTrue(aRecommendation.getRecommended().contains(productOneMock));
+		
+		
+	}
+
 
 }
