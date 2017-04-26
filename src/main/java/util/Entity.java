3ba43dac8dbc3f58f19cbf458a1bfaf769061c14
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-04-25 21:26:14 -0300

    Added - Entity class representing a persistent element for database

---
+++ src/main/java/util/Entity.java
@@ -0,0 +1,17 @@
+package util;
+
+public class Entity {
+	
+	private Integer id;
+	
+	
+	
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer newId) {
+		this.id = newId;
+	}
+	
+}
