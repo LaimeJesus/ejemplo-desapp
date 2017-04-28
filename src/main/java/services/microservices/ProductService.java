6cc7025d5ec27b37af6d6c2ff86f59477c07624b
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-04-28 17:10:29 -0300

    New - Added first microservice

---
+++ src/main/java/services/microservices/ProductService.java
@@ -0,0 +1,13 @@
+package services.microservices;
+
+import model.products.Product;
+
+public class ProductService extends GenericService<Product> {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8067303758622033038L;
+	
+}
