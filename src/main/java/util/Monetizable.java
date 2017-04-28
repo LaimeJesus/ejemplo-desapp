227a7fe607c5945ca95e785bd29bdb492f676a8b
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-04-28 17:00:24 -0300

    New - Monetizable Interface for Money - String convertion

---
+++ src/main/java/util/Monetizable.java
@@ -0,0 +1,18 @@
+package util;
+
+public interface Monetizable {
+
+	
+	
+	public Money getMonetizableElement();
+	
+	public void setMonetizableElement(Money newMoney);
+	
+	public default String getMoneyValue() {
+		return this.getMonetizableElement().toString();
+	}
+	
+	public default void setMoneyValue(String newValue) {
+		this.setMonetizableElement(Money.toMoney(newValue));
+	}
+}
