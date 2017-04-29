38985cfaf20283650e2df5942fd1d2c1776c84dc
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-04-29 20:04:20 -0300

    New - ProductList Service

---
+++ src/main/java/services/microservices/ProductListService.java
@@ -0,0 +1,12 @@
+package services.microservices;
+
+import model.products.ProductList;
+
+public class ProductListService extends GenericService<ProductList> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4298967256904633606L;
+
+}
