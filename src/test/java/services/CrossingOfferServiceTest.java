56ee22001b898b66149ab08e77673fe9bcb38b3f
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-04-29 20:07:17 -0300

    testCrossingOfferCanBeSaved

---
+++ src/test/java/services/CrossingOfferServiceTest.java
@@ -0,0 +1,33 @@
+package services;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import model.offers.CrossingOffer;
+import services.microservices.CrossingOfferService;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath*:/META-INF/spring-persistence-context.xml", "classpath*:/META-INF/spring-services-context.xml" })
+public class CrossingOfferServiceTest {
+
+	
+	
+	
+	
+	@Autowired
+    @Qualifier("services.microservices.crossingofferservice")
+    private CrossingOfferService crossingOfferService;
+
+    @Test
+    public void testCrossingOfferCanBeSaved(){
+    	crossingOfferService.save(new CrossingOffer());
+        Assert.assertEquals(1, crossingOfferService.retriveAll().size());
+    }
+	
+}
