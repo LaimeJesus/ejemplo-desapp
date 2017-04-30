d0ca87975724c0d798a81aa9a65bbdcf2fbdeded
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-04-29 21:09:10 -0300

    New - SelectedProduct Service

---
+++ src/main/java/services/microservices/SelectedProductService.java
@@ -0,0 +1,12 @@
+package services.microservices;
+
+import model.products.SelectedProduct;
+
+public class SelectedProductService extends GenericService<SelectedProduct>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8634847678853066903L;
+
+}
