c29c2562e965f488a67fa848465335259b68336f
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-04-29 21:08:59 -0300

    New - Recommendation Service

---
+++ src/main/java/services/microservices/RecommendationService.java
@@ -0,0 +1,12 @@
+package services.microservices;
+
+import model.recommendations.Recommendation;
+
+public class RecommendationService extends GenericService<Recommendation>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8735532598538235828L;
+
+}
