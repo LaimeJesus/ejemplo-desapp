eccafe0167ad4c8c788c4f5ea48a78f884c1a0f6
jesus <laimejesu@gmail.com>
2017-05-01 22:30:39 -0300

    added find a profile to profile service

--- src/main/java/services/microservices/ProfileService.java
+++ src/main/java/services/microservices/ProfileService.java
@@ -8,4 +8,8 @@ public class ProfileService extends GenericService<Profile>{
 	 */
 	private static final long serialVersionUID = -2834330268087404494L;
 
+	public Profile find(Profile aProfile) {
+		return this.getRepository().findByExample(aProfile).get(0);
+	}
+
 }
