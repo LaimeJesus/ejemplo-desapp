38adbf62c58cb40e341bd465b1219cd2dc146167
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-02 15:15:29 -0300

    testCategoryOfferCanBeSaved

---
+++ src/test/java/services/CategoryOfferServiceTest.java
@@ -0,0 +1,30 @@
+package services;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import model.offers.CategoryOffer;
+import services.microservices.CategoryOfferService;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath*:/META-INF/spring-persistence-context.xml", "classpath*:/META-INF/spring-services-context.xml" })
+public class CategoryOfferServiceTest {
+
+	
+	@Autowired
+    @Qualifier("services.microservices.categoryofferservice")
+    private CategoryOfferService categoryOfferService;
+
+    @Test
+    public void testCategoryOfferCanBeSaved(){
+    	categoryOfferService.save(new CategoryOffer());
+        Assert.assertEquals(1, categoryOfferService.retriveAll().size());
+    }
+	
+}
