94f7ed2ad5341fd2897e998338834f358d196e6d
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-02 15:12:48 -0300

    testCombinationOfferCanBeSaved

---
+++ src/test/java/services/CombinationOfferServiceTest.java
@@ -0,0 +1,30 @@
+package services;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import model.offers.CombinationOffer;
+import services.microservices.CombinationOfferService;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath*:/META-INF/spring-persistence-context.xml", "classpath*:/META-INF/spring-services-context.xml" })
+public class CombinationOfferServiceTest {
+
+	
+	@Autowired
+    @Qualifier("services.microservices.combinationofferservice")
+    private CombinationOfferService combinationOfferService;
+
+    @Test
+    public void testCombinationOfferCanBeSaved(){
+    	combinationOfferService.save(new CombinationOffer());
+        Assert.assertEquals(1, combinationOfferService.retriveAll().size());
+    }
+	
+}
