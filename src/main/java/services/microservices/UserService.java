d09446f1b6f49172488c313c6436352a54a3cd5f
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-04 12:51:37 -0300

    Added - Log In/Out SignUp and Authenticate methods

--- src/main/java/services/microservices/UserService.java
+++ src/main/java/services/microservices/UserService.java
@@ -1,5 +1,9 @@
 package services.microservices;
 
+import java.util.List;
+
+import exceptions.UserAlreadyExistsException;
+import exceptions.UsernameOrPasswordInvalidException;
 import model.users.User;
 
 public class UserService extends GenericService<User>{
@@ -9,4 +13,56 @@ public class UserService extends GenericService<User>{
 	 */
 	private static final long serialVersionUID = -2769328934018732341L;
 
+	
+	
+	
+	
+	public void createNewUser (User newUser) throws UserAlreadyExistsException {
+		this.validateNewUser(newUser);
+		this.save(newUser);
+	}
+	
+	public void loginUser (User user) throws UsernameOrPasswordInvalidException {
+		User possible = this.validateExist(user);
+		possible.login();
+		this.save(possible);
+	}
+	
+	public void logout (User user) throws UsernameOrPasswordInvalidException {
+		User possible = this.validateExist(user);
+		possible.logout();
+		this.save(possible);
+	}
+	
+	public User authenticateUser (User user) throws UsernameOrPasswordInvalidException {
+		return this.validateExist(user);
+	}
+	
+	public boolean hasWritePermission(User user) throws UsernameOrPasswordInvalidException {
+		User current = this.authenticateUser(user);
+		return current.hasWritePermission();
+		
+	}
+	
+	
+	
+	
+	public User findByUsername (String username) {
+		User exampleUser = new User();
+		exampleUser.setUsername(username);
+		List<User> possible = this.getRepository().findByExample(exampleUser);
+		return (possible.size() > 0) ? possible.get(0) : null;
+	}
+	
+	private User validateExist(User possible) throws UsernameOrPasswordInvalidException {
+		User exist = this.findByUsername(possible.getUsername());
+		if (! exist.equals(possible)) throw new UsernameOrPasswordInvalidException();
+		return exist;
+	}
+	
+	private void validateNewUser (User possibleNewUser) throws UserAlreadyExistsException {
+		User exist = this.findByUsername(possibleNewUser.getUsername());
+		if (exist != null) throw new UserAlreadyExistsException();
+	}
+	
 }
