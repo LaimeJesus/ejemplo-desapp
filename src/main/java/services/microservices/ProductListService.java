0b1c6dd1bb0921d7a905e1171e9f64238a8d90c6
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-06 15:04:27 -0300

    Added - Transactional Annotation for methods

--- src/main/java/services/microservices/ProductListService.java
+++ src/main/java/services/microservices/ProductListService.java
@@ -2,6 +2,9 @@ package services.microservices;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
+import exceptions.ProductDoesNotExistOnListException;
 import exceptions.ProductIsAlreadySelectedException;
 import model.products.Product;
 import model.products.ProductList;
@@ -17,23 +20,35 @@ public class ProductListService extends GenericService<ProductList> {
 	
 	
 	
-	
+	@Transactional
 	public void selectProduct( ProductList productList , Product product , Integer quantity) throws ProductIsAlreadySelectedException {
 		this.validateProductList(productList);
-		this.validateProduct(product);
+		if (this.validateProduct(product)) {
+			List<ProductList> possible = this.getRepository().findByExample(productList);
+			ProductList result = possible.get(0);
+			result.selectProduct(product, quantity);
+			this.update(result);
+		}
+	}	
+	
+	@Transactional
+	public void removeProduct(ProductList productList, Product product) throws ProductDoesNotExistOnListException {
+		if (this.validateProduct(product) ) {
+			List<ProductList> possible = this.getRepository().findByExample(productList);
+			ProductList result = possible.get(0);
+			result.removeProduct(product);
+			this.update(result);
+		}
 		
-		List<ProductList> possible = this.getRepository().findByExample(productList);
-		ProductList result = possible.get(0);
-		result.selectProduct(product, quantity);
-		this.update(result);		
 	}
 	
 	private void validateProductList(ProductList aProductList) {
 		//TODO
 	}
 	
-	private void validateProduct(Product aProduct) {
-		productService.retriveAll().contains(aProduct);
+	@Transactional
+	private boolean validateProduct(Product aProduct) {
+		return productService.retriveAll().contains(aProduct);
 	}
 
 	
@@ -46,5 +61,4 @@ public class ProductListService extends GenericService<ProductList> {
 	public void setProductService(ProductService productService) {
 		this.productService = productService;
 	}
-	
 }
