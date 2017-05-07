8a1ed1be293771bc04dcb2344e3cce884af7ec79
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-07 17:57:40 -0300

    Added - List of Applicable Offer for a productList for CombinationOfferService

--- src/main/java/services/microservices/CombinationOfferService.java
+++ src/main/java/services/microservices/CombinationOfferService.java
@@ -1,6 +1,10 @@
 package services.microservices;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.offers.CombinationOffer;
+import model.products.ProductList;
 
 public class CombinationOfferService extends GenericService<CombinationOffer> {
 
@@ -9,4 +13,15 @@ public class CombinationOfferService extends GenericService<CombinationOffer> {
 	 */
 	private static final long serialVersionUID = -6264043641769715489L;
 
+	public List<CombinationOffer> applicableForList(ProductList aProductList) {
+		List<CombinationOffer> results = new ArrayList<CombinationOffer>();
+		List<CombinationOffer> possibles = this.retriveAll();
+		for (CombinationOffer offer : possibles) {
+			if (offer.meetRequirements(aProductList)){
+				results.add(offer);
+			}
+		}
+		return results;
+	}
+	
 }
