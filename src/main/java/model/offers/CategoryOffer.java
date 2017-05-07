16d1579b78818cbea7167eaa22fdd752d5a69f05
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-07 17:55:46 -0300

    Added - Equals Implementation Fixed - getPreviousPrice was giving wrong value

--- src/main/java/model/offers/CategoryOffer.java
+++ src/main/java/model/offers/CategoryOffer.java
@@ -1,8 +1,11 @@
 package model.offers;
 
+import java.util.List;
+
 import org.joda.time.Interval;
 
 import model.products.ProductList;
+import model.products.SelectedProduct;
 import util.Category;
 import util.Money;
 
@@ -21,7 +24,12 @@ public class CategoryOffer extends Offer {
 
 	@Override
 	public Money getPreviousPrice(ProductList productList) {
-		return productList.getTotalAmount();
+		List<SelectedProduct> list = productList.getSelectedProductsBy(this.getCategory());
+		Money result = new Money(0,0);
+		for (SelectedProduct current : list) {
+			result = result.add( current.getProduct().getPrice().times(current.getQuantity()) );
+		}
+ 		return result;
 	}
 
 	@Override
@@ -42,6 +50,27 @@ public class CategoryOffer extends Offer {
 	
 	public void setCategory(Category newCategory) {
 		this.category = newCategory;
+	}
+	
+	
+	@Override
+	public boolean equals(Object anotherPossibleOffer ) {
+		if(this.isMyType(anotherPossibleOffer)){
+			CategoryOffer newOffer = (CategoryOffer) anotherPossibleOffer;
+			return this.totalEquals(newOffer);
+		}
+		return false;
+	}
+	
+	private boolean isMyType(Object anotherPossibleOffer) {
+		return anotherPossibleOffer != null && anotherPossibleOffer instanceof CategoryOffer;
+	}
+
+	public boolean totalEquals( CategoryOffer someOffer ) {
+		return 
+			this.getDiscountRate().equals(someOffer.getDiscountRate()) &&
+			this.getValidPeriod().equals(someOffer.getValidPeriod()) &&
+			this.getCategory().equals(someOffer.getCategory());
 	}
 	
 }
