a65a51ec36f8e2e99aefa555a2032746d1ceaa7e
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-07 17:56:13 -0300

    Added - List of Offer applicable for a productList

--- src/main/java/services/microservices/CategoryOfferService.java
+++ src/main/java/services/microservices/CategoryOfferService.java
@@ -1,6 +1,10 @@
 package services.microservices;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.offers.CategoryOffer;
+import model.products.ProductList;
 
 public class CategoryOfferService extends GenericService<CategoryOffer> {
 
@@ -8,5 +12,16 @@ public class CategoryOfferService extends GenericService<CategoryOffer> {
 	 * 
 	 */
 	private static final long serialVersionUID = -6525564083052739423L;
+
+	public List<CategoryOffer> applicableForList(ProductList aProductList) {
+		List<CategoryOffer> results = new ArrayList<CategoryOffer>();
+		List<CategoryOffer> possibles = this.retriveAll();
+		for (CategoryOffer offer : possibles) {
+			if (offer.meetRequirements(aProductList)){
+				results.add(offer);
+			}
+		}
+		return results;
+	}
 
 }
