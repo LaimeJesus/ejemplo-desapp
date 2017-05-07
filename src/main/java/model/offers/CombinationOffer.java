66c6b19bf9fc0ec3edfd0fae807673ccab05b20b
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-07 17:57:02 -0300

    Added - Equals Implementation for CombinationOffer

--- src/main/java/model/offers/CombinationOffer.java
+++ src/main/java/model/offers/CombinationOffer.java
@@ -53,5 +53,25 @@ public class CombinationOffer extends Offer {
 		this.combinatedProduct = aNewCombinatedProduct;
 	}
 
+	@Override
+	public boolean equals(Object anotherPossibleOffer ) {
+		if(this.isMyType(anotherPossibleOffer)){
+			CombinationOffer newOffer = (CombinationOffer) anotherPossibleOffer;
+			return this.totalEquals(newOffer);
+		}
+		return false;
+	}
+	
+	private boolean isMyType(Object anotherPossibleOffer) {
+		return anotherPossibleOffer != null && anotherPossibleOffer instanceof CombinationOffer;
+	}
+
+	public boolean totalEquals( CombinationOffer someOffer ) {
+		return
+			this.getDiscountRate().equals(someOffer.getDiscountRate()) &&
+			this.getValidPeriod().equals(someOffer.getValidPeriod()) &&
+			this.getRelatedProduct().equals(someOffer.getRelatedProduct()) &&
+			this.getCombinatedProduct().equals(someOffer.getCombinatedProduct());
+	}
 
 }
