438acad8a50b0aa42da91eabeb3cd88fdbaac0c6
sandoval.lucasj@gmail.com <sandoval.lucasj@gmail.com>
2017-05-11 11:18:16 -0300

    Fixed - test where using the wrong idea - see testWhenWorkingWithListsMoneysArenPersisted commit

--- src/test/java/model/offers/CategoryOfferTest.java
+++ src/test/java/model/offers/CategoryOfferTest.java
@@ -2,6 +2,9 @@ package model.offers;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.joda.time.DateTime;
 import org.joda.time.Interval;
 import org.junit.Test;
@@ -11,6 +14,7 @@ import exceptions.ProductIsAlreadySelectedException;
 import model.offers.CategoryOffer;
 import model.products.Product;
 import model.products.ProductList;
+import model.products.SelectedProduct;
 import util.Category;
 import util.Money;
 
@@ -20,7 +24,29 @@ public class CategoryOfferTest {
 	public void testWhenTheCategoryOfferNeedThePrevioysPriceThenAsksToProductList() {
 		
 		ProductList aProductListMock = Mockito.mock(ProductList.class);
-		Mockito.when(aProductListMock.getTotalAmount()).thenReturn(new Money(156,76));
+		Product aProductMock1 = Mockito.mock(Product.class);
+		Product aProductMock2 = Mockito.mock(Product.class);
+		
+		Mockito.when(aProductMock1.getCategory()).thenReturn(Category.Baked);
+		Mockito.when(aProductMock2.getCategory()).thenReturn(Category.Fruit);
+		
+		Mockito.when(aProductMock1.getPrice()).thenReturn(new Money(12,30));
+		Mockito.when(aProductMock2.getPrice()).thenReturn(new Money(52,30));
+		
+		SelectedProduct aSelectedProductMock1 = Mockito.mock(SelectedProduct.class);
+		SelectedProduct aSelectedProductMock2 = Mockito.mock(SelectedProduct.class);
+		
+		Mockito.when(aSelectedProductMock1.getProduct()).thenReturn(aProductMock1);
+		Mockito.when(aSelectedProductMock2.getProduct()).thenReturn(aProductMock2);
+		Mockito.when(aSelectedProductMock1.getFinalPrice()).thenReturn(new Money(3,50));
+		Mockito.when(aSelectedProductMock2.getFinalPrice()).thenReturn(new Money(27,25));
+		
+		List<SelectedProduct> selected = new ArrayList<SelectedProduct>();
+		selected.add(aSelectedProductMock1);
+		selected.add(aSelectedProductMock2);
+		
+		Mockito.when(aProductListMock.getTotalAmount()).thenReturn(new Money(24,60));
+		Mockito.when(aProductListMock.getAllProducts()).thenReturn(selected);
 		
 		DateTime today = DateTime.now();
 		DateTime tomorrow = today.plusDays(1);
@@ -28,7 +54,7 @@ public class CategoryOfferTest {
 		
 		CategoryOffer aCategoryOffer = new CategoryOffer(15, anInterval, Category.Baked);
 		
-		Money expected = new Money(156,76);
+		Money expected = new Money(3,50);
 		
 		assertEquals(expected, aCategoryOffer.getPreviousPrice(aProductListMock));
 		
