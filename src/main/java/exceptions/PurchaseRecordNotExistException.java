cb2005a371a32ede63837551a15dc7db34955ced
jesus <laimejesu@gmail.com>
2017-06-01 09:53:35 -0300

    added exceptions for purchase record not exist and user not exist

---
+++ src/main/java/exceptions/PurchaseRecordNotExistException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class PurchaseRecordNotExistException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8755933967650253053L;
+
+}
---
+++ src/main/java/exceptions/UserDoesNotExistException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class UserDoesNotExistException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1398946747507327275L;
+
+}
