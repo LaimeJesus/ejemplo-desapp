49ebe4d6e405627b10d8721a4d989e132e68400d
jesus <laimejesu@gmail.com>
2017-06-03 19:27:35 -0300

    added create and delete endpoints, ex: create product list, create selectedproduct

--- src/main/java/rest/controllers/UsersController.java
+++ src/main/java/rest/controllers/UsersController.java
@@ -17,14 +17,21 @@ import javax.ws.rs.core.Response.Status;
 
 import com.google.gson.JsonSyntaxException;
 
+import exceptions.ProductIsAlreadySelectedException;
 import exceptions.ProductListNotExistException;
+import exceptions.ProductNotExistException;
 import exceptions.PurchaseRecordNotExistException;
 import exceptions.SelectedProductNotExistException;
 import exceptions.UserAlreadyExistsException;
 import exceptions.UserDoesNotExistException;
+import exceptions.UserIsNotLoggedException;
+import exceptions.UsernameDoesNotExistException;
+import exceptions.UsernameOrPasswordInvalidException;
+import model.products.ProductList;
 import model.users.User;
 import rest.dtos.ProfileDTO;
 import rest.dtos.ResponseDTO;
+import rest.dtos.SelectedProductDTO;
 import rest.dtos.UserDTO;
 import services.general.GeneralService;
 import util.Address;
@@ -60,11 +67,12 @@ public class UsersController {
 	@POST
 	@Path("/")
 	@Consumes("application/json")
+	@Produces("application/json")
 	public Response createUser(String userJson){
 		try {
-			User user = responseDTO.gson.fromJson(userJson, User.class);
-			generalService.createUser(user);
-			return responseDTO.ok(user);
+			User u = responseDTO.gson.fromJson(userJson, User.class);
+			generalService.createUser(u);
+			return responseDTO.ok(generalService.getUserService().findByUsername(u.getUsername()));
 		} catch (JsonSyntaxException | UserAlreadyExistsException e) {
 			return responseDTO.error(Status.CONFLICT, e.getMessage());
 		}		
@@ -127,7 +135,7 @@ public class UsersController {
 		}
 	}
 
-	//ESTE ESTA MAL, tengo q revisarlo
+	//END POINTS NO FUNCIONA, tengo q revisarlo
 	@PUT
 	@Path("/{id}/profile")
 	public Response createOrUpdateProfile(@PathParam("id") Integer id, ProfileDTO profile){
@@ -161,7 +169,7 @@ public class UsersController {
 			return responseDTO.error(Status.INTERNAL_SERVER_ERROR, e.getMessage());
 		}
 	}
-
+	
 	/////////////////////////////////////////////////////////////////////////////////
 	//ADDRESS METHODS OF AN USER
 	@GET
@@ -197,26 +205,62 @@ public class UsersController {
 			return responseDTO.error(Status.CONFLICT, e.getMessage());
 		}
 	}
+	@POST
+	@Path("/{userId}/productlists/")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response createProductList(@PathParam("userId") Integer userId, String productListJson){
+		try {
+			generalService.createProductList(userId, responseDTO.gson.fromJson(productListJson, ProductList.class));
+			return responseDTO.ok("list created");
+		} catch (UserDoesNotExistException | UserIsNotLoggedException e) {
+			return responseDTO.error(Status.CONFLICT, e.getMessage());
+		}
+	}
+	@DELETE
+	@Path("/{userId}/productlists/{productlistId}")
+	public Response deleteProductListById(@PathParam("userId") Integer userId, @PathParam("productlistId") Integer productlistId){
+		try {
+			generalService.deleteProductList(userId, productlistId);
+			return responseDTO.ok("product list deleted");
+		} catch (UserDoesNotExistException | ProductListNotExistException e) {
+			return responseDTO.error(Status.CONFLICT, e.getMessage());
+		}
+	}
 	
 	/////////////////////////////////////////////////////////////////////////////////
 	//SELECT PRODUCT METHODS OF A LIST OF AN USER
 	@GET
-	@Path("/{userId}/productlists/{listId}/selectedproducts")
+	@Path("/{userId}/productlists/{productlistId}/selectedproducts")
 	@Produces("application/json")
-	public Response getSelectedProducts(@PathParam("userId") Integer userId, @PathParam("listId") Integer listId){
+	public Response getSelectedProducts(@PathParam("userId") Integer userId, @PathParam("productlistId") Integer productlistId){
 		try {
-			return responseDTO.ok(generalService.getProductListById(userId, listId).getAllProducts());
+			return responseDTO.ok(generalService.getProductListById(userId, productlistId).getAllProducts());
 		} catch (ProductListNotExistException | UserDoesNotExistException e) {
 			return responseDTO.error(Status.CONFLICT, e.getMessage());
 		}
 	}
 	@GET
-	@Path("/{userId}/productlists/{listId}/selectedproducts/{selectproductId}")
+	@Path("/{userId}/productlists/{productlistId}/selectedproducts/{selectproductId}")
 	@Produces("application/json")
-	public Response getSelectedProducts(@PathParam("userId") Integer userId, @PathParam("listId") Integer listId, @PathParam("selectproductId") Integer selectProductId){
+	public Response getSelectedProduct(@PathParam("userId") Integer userId, @PathParam("listId") Integer listId, @PathParam("selectproductId") Integer selectProductId){
 		try {
 			return responseDTO.ok(generalService.getProductListById(userId, listId).getSelectedProduct(selectProductId));
 		} catch (ProductListNotExistException | UserDoesNotExistException | SelectedProductNotExistException e) {
+			return responseDTO.error(Status.CONFLICT, e.getMessage());
+		}
+	}
+	@POST
+	@Path("/{userId}/productlists/{productlistId}/selectedproducts")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response createSelectedProduct(@PathParam("userId") Integer userId, @PathParam("productlistId") Integer productlistId, String selectedProductJson){
+		try {
+			SelectedProductDTO selectedProduct = responseDTO.gson.fromJson(selectedProductJson, SelectedProductDTO.class);
+			generalService.createSelectedProduct(userId, productlistId, selectedProduct.productId, selectedProduct.quantity);
+			return responseDTO.ok("product selected");
+		} catch (UserDoesNotExistException | UserIsNotLoggedException | ProductListNotExistException
+				| ProductNotExistException | ProductIsAlreadySelectedException | JsonSyntaxException e) {
 			return responseDTO.error(Status.CONFLICT, e.getMessage());
 		}
 	}
@@ -224,11 +268,11 @@ public class UsersController {
 	/////////////////////////////////////////////////////////////////////////////////
 	//OFFERS APPLIED OF A LIST OF AN USER METHODS
 	@GET
-	@Path("/{userId}/productlists/{listId}/offers")
+	@Path("/{userId}/productlists/{productlistId}/offers")
 	@Produces("application/json")
-	public Response getOffers(@PathParam("userId") Integer userId, @PathParam("listId") Integer listId){
+	public Response getOffers(@PathParam("userId") Integer userId, @PathParam("productlistId") Integer productlistId){
 		try {
-			return responseDTO.ok(generalService.getProductListById(userId, listId).getAppliedOffers());
+			return responseDTO.ok(generalService.getProductListById(userId, productlistId).getAppliedOffers());
 		} catch (ProductListNotExistException | UserDoesNotExistException e) {
 			return responseDTO.error(Status.CONFLICT, e.getMessage());
 		}
@@ -240,12 +284,24 @@ public class UsersController {
 	public Response create(UserDTO user){
 		try {
 			generalService.createUser(user.fullUser());
-			return Response.ok().build();
+			return responseDTO.ok("user created");
 		} catch (UserAlreadyExistsException e) {
 			return responseDTO.error(Status.FORBIDDEN, e.getMessage());
 		}
 	}
 
+	@POST
+	@Path("/login")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response login(UserDTO user){
+		try {
+			generalService.loginUser(user.fullUser());
+			return responseDTO.ok(generalService.getUserService().findByUsername(user.username));
+		} catch (UsernameDoesNotExistException | UsernameOrPasswordInvalidException e) {
+			return responseDTO.error(Status.CONFLICT, e.getMessage());
+		}
+	}
 	
 
 }
