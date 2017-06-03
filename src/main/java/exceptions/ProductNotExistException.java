366411e9e1b41f4ffc389beb49269fba152340f8
jesus <laimejesu@gmail.com>
2017-06-03 10:07:24 -0300

    added validation product not exist

---
+++ src/main/java/exceptions/ProductNotExistException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class ProductNotExistException extends Exception {
+
+	public ProductNotExistException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -295416680901765144L;
+
+}
