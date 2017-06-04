9b48953326228588e200992d0cbeb8bc0ea1c2e3
jesus <laimejesu@gmail.com>
2017-06-04 11:22:24 -0300

    created crossing offer does not exist exception

---
+++ src/main/java/exceptions/CrossingOfferNotExistException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class CrossingOfferNotExistException extends Exception {
+
+	public CrossingOfferNotExistException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3712649889580016834L;
+
+}
